85a99584e06c29096b5961af61f78c19848f0cf6
Aymeric Conti <aymeric.conti2002@gmail.com>
2024-12-17 11:03:02 +0100

    profiling

--- agents/tensors/tensor.py
+++ agents/tensors/tensor.py
@@ -2,6 +2,8 @@ import numpy as np
 import torch
 from agents.lux.utils import Tiles
 from src.luxai_s3.state import EnvObs
+from jax.dlpack import to_dlpack
+from torch.utils.dlpack import from_dlpack
 
 
 class TensorConverter:
@@ -25,34 +27,17 @@ class TensorConverter:
             dtype=torch.float32,
         )
 
-        tensor[0] = torch.tensor(
-            ~np.array(obs.sensor_mask, dtype=bool), dtype=torch.float32
-        )
-        tensor[1] = torch.tensor(
-            np.array(obs.map_features.tile_type, dtype=np.int32) == Tiles.ASTEROID,
-            dtype=torch.float32,
-        )
-        tensor[2] = torch.tensor(
-            np.array(obs.map_features.tile_type, dtype=np.int32) == Tiles.NEBULA,
-            dtype=torch.float32,
-        )
+        tensor[0] = from_dlpack(to_dlpack(~obs.sensor_mask))
+        tensor[1] = from_dlpack(to_dlpack(obs.map_features.tile_type == Tiles.ASTEROID))
+        tensor[2] = from_dlpack(to_dlpack(obs.map_features.tile_type == Tiles.NEBULA))
         for relic_id in obs.get_avaible_relics():
             x = obs.relic_nodes[relic_id][0].item()
             y = obs.relic_nodes[relic_id][1].item()
             tensor[3, x, y] = 1
-        tensor[4] = torch.tensor(
-            np.array(obs.map_features.energy, dtype=np.float32) / 20,
-            dtype=torch.float32,
-        )
+        tensor[4] = from_dlpack(to_dlpack(obs.map_features.energy / 20))
         for id in obs.get_avaible_units(1 - team_id):
-            tensor[5] += torch.tensor(
-                np.array(obs.units.energy[1 - team_id][id], dtype=np.float32) / 400,
-                dtype=torch.float32,
-            )
+            tensor[5] += from_dlpack(to_dlpack(obs.units.energy[1 - team_id][id] / 400))
         for id in obs.get_avaible_units(team_id):
-            tensor[6 + id] = torch.tensor(
-                np.array(obs.units.energy[team_id][id], dtype=np.float32) / 400,
-                dtype=torch.float32,
-            )
+            tensor[6 + id] = from_dlpack(to_dlpack(obs.units.energy[team_id][id] / 400))
 
         return tensor
