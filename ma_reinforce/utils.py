519efc03cfafa8a92bb395ec185a7aac61f6c35e
Hadrien Crassous <hadrien.crassous@student-cs.fr>
2024-12-18 11:01:01 +0100

    created custom_env and utils

---
+++ ma_reinforce/utils.py
@@ -0,0 +1,20 @@
+
+def is_alive_mask(obs, team_id):
+    return obs.units_mask[team_id]
+
+
+def relic_reward(team_id: int, previous_obs, obs):
+    pass
+
+def sapped_reward(team_id: int, previous_obs, obs, actions_of_team):
+    pass 
+
+def reveal_reward(team_id: int, previous_obs, obs):
+    pass
+
+def high_energy_reward(team_id: int, previous_obs, obs):
+    pass
+
+def death_reward(team_id: int, previous_obs, obs):
+    pass
+
